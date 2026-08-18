6491a092146f5d05b8b783f86b37290e54858c07
fabius <fabius2001@hotmail.de>
2026-08-18 14:33:28 +0200

    Added yubikey ssh to workstation

--- hosts/workstation/default.nix
+++ hosts/workstation/default.nix
@@ -27,6 +27,10 @@
     v4l-utils
   ];
 
+  local.primaryUser.authorizedKeys = [
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDPgm7K7t41fVBh5Jajl4+vPyJO7jA45U8soik3sgL4kAAAACnNzaDpmYWJpdXM= fabius yubikey ssh"
+  ];
+
   # Dedicated login for Nix remote builds from small LAN machines such as
   # miix310. Add the miix310-generated public key here after creating it.
   users.users.nixremote = {
